a7bc4c1d9942c404a9b96782bd399983b20cc6d5
Sam Harwell <sam@tunnelvisionlabs.com>
2015-02-09 05:47:03 -0600

    [wip] Support the JDWP in addition to the custom WCF-based transport

---
+++ Tvl.Java.DebugInterface.Client/Jdwp/JdwpDebugProtocolService.cs
@@ -0,0 +1,21 @@
+﻿namespace Tvl.Java.DebugInterface.Client.Jdwp
+{
+    public class JdwpDebugProtocolService : IDebugProtocolService
+    {
+        private const int HeaderSize = 11;
+
+        private readonly IDebugProtocolServiceCallback _callback;
+
+        private readonly ConcurrentBag<int> _messageIds = new ConcurrentBag<int>();
+
+        private int _nextMessageId = 1;
+
+        public JdwpDebugProtocolService(IDebugProtocolServiceCallback callback)
+        {
+            if (callback == null)
+                throw new ArgumentNullException("callback");
+
+            _callback = callback;
+        }
+    }
+}
